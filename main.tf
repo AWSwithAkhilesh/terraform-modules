b58cc8202b537467a8968b394c03ac932b33992e
AWSwithAkhilesh <akhil200186@gmail.com>
2026-04-28 17:33:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ module "vpc" {
 
 module "ec2" {
   source        = "./modules/ec2-instances"
-  ami           = "ami-02ddb77f8f93ca4ca"       # Amazon Linux 2023 in ap-south-1
+  ami           = "ami-0e12ffc2dd465f6e4"       # Amazon Linux 2023 in ap-south-1
   instance_type = "t3.micro"
   subnet_id     = module.vpc.public_subnet_id  # public_subnet_id is a output of VPC module output.tf. Output of VPC module is the input here
 }
 
 module "s3" {
   source      = "./modules/s3-bucket"
-  bucket_name = "my-demo-bucket-bkt-test-1"     # Change to a unique bucket name
+  bucket_name = "my-demo-bucket-bkt-test-1818"     # Change to a unique bucket name
 }
 output "instance_id" {
   value = module.ec2.instance_id
